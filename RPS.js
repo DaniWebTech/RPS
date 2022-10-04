f0df0a4e92ca843cc070cb179f4797b3a44f8ae7
DaniWebTech <webtechenglish@gmail.com>
2022-10-04 11:47:10 +0200

    Added GetComputerChoice function to RPS.js

---
+++ RPS.js
@@ -0,0 +1,14 @@
+//Get computer Choice
+
+const myArray =  ["Rock", "Paper", "Scissors"];
+
+function getComputerChoice(myArray) {
+   console.log (myArray[Math.floor(Math.random()*myArray.length)]);
+
+}
+
+getComputerChoice(myArray)
+
+
+
+
